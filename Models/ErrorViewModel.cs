f3bc70844fe65fcddd1d4008e437de9c304fcac7
Zameer Siddiqui <zameersq03@gmail.com>
2024-08-10 03:55:41 +0530

    Initial Commit.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Inventory_Insight.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
